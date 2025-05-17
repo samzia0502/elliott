7e51a1c9aa3a52b0bb6d742f3d3abe5df2432ad4
samzia0502 <marketX.in@gmail.com>
2025-05-18 00:07:51 +0530

    Update wave_dashboard.py

--- wave_dashboard.py
+++ wave_dashboard.py
@@ -1,5 +1,3 @@
-Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
